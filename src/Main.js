5aefe40821e5673272f853c34d3750bc1adb8c1f
Andrew M <andrewlmanley@gmail.com>
2016-09-01 18:47:58 -0400

    -added restify library. -added static file serving with restify -removed 'use_strict' launch option since it was causing restify to break.

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,37 @@
 const path = require("path");
-const Logger = require("./logger/Logger");
+const fs = require("fs");
+const logger = require("./logger/Logger");
+const restify = require("restify");
+const environment = process.env.NODE_ENV || "development";
 
-Logger.Initialize(path.join(__dirname, "../logs"));
+logger.Initialize(path.join(__dirname, "../logs"));
+logger.LogInfo(`Server is running in '${environment}' environment`)
 
-Logger.LogInfo("WHATS UP");
-Logger.LogInfoData("WHATS UP BREH", {p: "oop"});
+var server;
+
+if(environment == "production") 
+{
+  server = restify.createServer({
+    certificate: fs.readFileSync("path/to/server/certificate"),
+    key: fs.readFileSync("path/to/server/key"),
+    name: "Botanist",
+  });
+
+  server.listen(453);
+}
+else
+{
+  server = restify.createServer({
+    name: "Botanist"
+  });
+
+  server.listen(80);
+}
+
+logger.LogInfo(`Server name: ${server.name} Server url: ${server.url}`);
+
+//serving static files
+server.get(/.*/, restify.serveStatic({
+  "directory": path.join(__dirname, "../public"),
+  "default": "index.html"
+ }));
