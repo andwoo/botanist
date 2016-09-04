cdde5f08970cb65d3d37792a397ab480ada9a946
Andrew <andrewlmanley@gmail.com>
2016-09-04 16:37:10 -0400

    -added error reporting back to user if definition isnt found. -added better definition formatting for channel.

--- src/commands/UrbanDictionaryCommand.js
+++ src/commands/UrbanDictionaryCommand.js
@@ -10,45 +10,60 @@ module.exports.HandleRequest = function(request, response, next){
     if(request.params.text && request.params.text.length > 0) {
       logger.LogInfo(`Looking up definition ${request.params.text}`);
       urban(request.params.text).first(function(json) {
-        response.json(200, {
-          response_type: "in_channel",
-          text: json.definition,
-          attachments: [
-            {
-              text: json.example
-            },
-            {
-              text: json.permalink
-            }
-          ]
-        });
+        if(json) {
+          response.json(200, FormatSlackAttachmentMessage(true, json));
+        }
+        else {
+          response.json(200, FormatSlackMessage(false, `Could not find a definition for ${request.params.text}.`));
+        }
       });
     }
     else {
       logger.LogInfo("Looking up random definition");
       urban.random().first(function(json) {
-        response.json(200, {
-          response_type: "in_channel",
-          text: json.definition,
-          attachments: [
-            {
-              text: json.example
-            },
-            {
-              text: json.permalink
-            }
-          ]
-        });
+        if(json) {
+          response.json(200, FormatSlackAttachmentMessage(true, json));
+        }
+        else {
+          response.json(200, FormatSlackMessage(false, "Could not find a random definition."));
+        }
       });
     }
-
-
-
-
-    // response.json(200, {
-    //   response_type: "in_channel",
-    //   text: "Slack's POST data: " + JSON.stringify(request.params)
-    // });
   }
   return next();
 };
+
+function FormatSlackAttachmentMessage(sendToChannel, jsonData) {
+  return {
+    response_type: sendToChannel ? "in_channel" : "ephemeral",
+    text: undefined,
+    attachments: [{
+      title: jsonData.title,
+      title_link: jsonData.permalink,
+      text: jsonData.definition
+    },
+    {
+      text: `Example:\n ${jsonData.example}`
+    }]
+  }
+}
+
+function FormatSlackMessage(sendToChannel, text) {
+  return {
+    response_type: sendToChannel ? "in_channel" : "ephemeral",
+    text: text
+  }
+}
+
+//example
+// {
+// 	definition: 'The ultimate self promoter. He makes good beats but barely says anything in his songs.  He always wants you to listen, but does not have alot to say. Anywhere you see him, either on tv or on a truck or billboard he will be sayin- buy my album.  \r\n \r\n',
+// 	permalink: 'http://dj-khaled.urbanup.com/2561463',
+// 	thumbs_up: 970,
+// 	author: 'Tdowns',
+// 	word: 'DJ khaled',
+// 	defid: 2561463,
+// 	current_vote: '',
+// 	example: 'Dj Khaled:  Lisssttennnn\r\nMe: im listening\r\nDj Khaled: We da best!',
+// 	thumbs_down: 330
+// }
